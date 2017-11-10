85aa1445b7547a01821c89ecc8f5019157d09621
louxiujun <louxiujun@corp.netease.com>
2017-11-10 17:17:08 +0800

    modefied some detail

---
+++ spring-mvc-hello/src/com/netease/hello/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.netease.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by louxj424 on 2017/10/27.
+ */
+
+@Controller
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("msg", "Spring MVC Hello World");
+        model.addAttribute("name", "yuntao");
+        return "hello";
+    }
+}
---
+++ spring-mvc-hello/web/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: louxj424
+  Date: 2017/10/27
+  Time: 下午5:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>${msg}</title>
+</head>
+<body>
+<h1>${msg}</h1>
+<span>${name}</span>
+</body>
+</html>
